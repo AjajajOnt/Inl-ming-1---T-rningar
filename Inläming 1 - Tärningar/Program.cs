a7971f893931d9983f91f311f9d27a2e6c26c951
AjajajOnt <alioch005@hotmail.com>
2021-09-18 15:46:45 +0200

    Test2

--- Inläming 1 - Tärningar/Program.cs
+++ Inläming 1 - Tärningar/Program.cs
@@ -12,17 +12,25 @@ namespace Inläming_1___Tärningar
             int tärning2 = rnd.Next(1,6);
             int tärning3 = rnd.Next(1,6);
             int ValtLyckoTal = 0;
+            Console.WriteLine("Välj ett lyckotal mellan 1 och 6");
             ValtLyckoTal = int.Parse(Console.ReadLine());
+            int SatsadePix = 0;
+            Console.WriteLine("Satsa Pix");
+            SatsadePix = int.Parse(Console.ReadLine());
 
             if (tärning1 == ValtLyckoTal || tärning2 == ValtLyckoTal || tärning3 == ValtLyckoTal)
             {
+                SatsadePix = SatsadePix *= 2;
+                pix = pix += SatsadePix;
+                Console.WriteLine("Du har Vunnit " + "Pix kvar: " + pix);
 
-                Console.WriteLine("Du har Vunnit");
 
             }
             else
             {
-                Console.WriteLine("Du har förlorat");
+                pix = pix -= SatsadePix;
+                Console.WriteLine("Du har förlorat " + "Pix kvar: " + pix);
+
             }
 
         }
