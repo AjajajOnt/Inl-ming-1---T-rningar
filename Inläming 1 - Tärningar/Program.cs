733a05ddfbc71d9e6b7a4854f953e2207c4daf8e
AjajajOnt <alioch005@hotmail.com>
2021-09-18 15:27:37 +0200

    massa ints

--- Inläming 1 - Tärningar/Program.cs
+++ Inläming 1 - Tärningar/Program.cs
@@ -6,7 +6,14 @@ namespace Inläming_1___Tärningar
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
             int pix = 500;
+            int tärning1 = rnd.Next(1,6);
+            int tärning2 = rnd.Next(1,6);
+            int tärning3 = rnd.Next(1,6);
+            int ValtLyckoTal = 0;
+            ValtLyckoTal = int.Parse(Console.ReadLine());
+
         }
     }
 }
