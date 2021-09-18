96f7510e9d404894e9a74129ce1a499885e826e5
AjajajOnt <alioch005@hotmail.com>
2021-09-18 17:03:04 +0200

    While Sats

--- Inläming 1 - Tärningar/Program.cs
+++ Inläming 1 - Tärningar/Program.cs
@@ -8,79 +8,81 @@ namespace Inläming_1___Tärningar
         {
             Random rnd = new Random();
             int pix = 500;
-            int tärning1 = rnd.Next(1,6);
-            int tärning2 = rnd.Next(1,6);
-            int tärning3 = rnd.Next(1,6);
             int ValtLyckoTal = 0;
-            Console.WriteLine("Välj ett lyckotal mellan 1 och 6");
-            ValtLyckoTal = int.Parse(Console.ReadLine());
             int SatsadePix = 0;
-            Console.WriteLine("Satsa Pix");
-            SatsadePix = int.Parse(Console.ReadLine());
 
 
-            if (SatsadePix >= 50 && SatsadePix <= 500 && ValtLyckoTal >= 1 && ValtLyckoTal <= 6)
+            while (pix >= 50)
             {
 
-                if (tärning1 == ValtLyckoTal && tärning2 == ValtLyckoTal && tärning3 == ValtLyckoTal)
+                int tärning1 = rnd.Next(1, 6);
+                int tärning2 = rnd.Next(1, 6);
+                int tärning3 = rnd.Next(1, 6);
+                Console.WriteLine("Välj ett lyckotal mellan 1 och 6");
+                ValtLyckoTal = int.Parse(Console.ReadLine());
+                Console.WriteLine("Satsa Pix");
+                SatsadePix = int.Parse(Console.ReadLine());
+
+                if (SatsadePix >= 50 && SatsadePix <= pix && ValtLyckoTal >= 1 && ValtLyckoTal <= 6)
+
                 {
 
-                    SatsadePix = SatsadePix *= 4;
-                    pix = pix += SatsadePix;
+                    if (tärning1 == ValtLyckoTal && tärning2 == ValtLyckoTal && tärning3 == ValtLyckoTal)
+                    {
 
-                    Console.WriteLine("Du har Vunnit 4x " + "Pix kvar: " + pix);
-                    Console.WriteLine("Tärning 1; " + tärning1 + " Tärning2; " + tärning2 + " Tärning3; " + tärning2);
+                        SatsadePix = SatsadePix *= 4;
+                        pix = pix += SatsadePix;
+
+                        Console.WriteLine("Du har Vunnit 4x " + "Pix kvar: " + pix);
+                        Console.WriteLine("Tärning 1; " + tärning1 + " Tärning2; " + tärning2 + " Tärning3; " + tärning2);
 
 
-                }
+                    }
 
-                else if (tärning1 == ValtLyckoTal && tärning2 == ValtLyckoTal || tärning2 == ValtLyckoTal && tärning3 == ValtLyckoTal || tärning3 == ValtLyckoTal &&  tärning2 == ValtLyckoTal || tärning3 == ValtLyckoTal && tärning3 == ValtLyckoTal)
-                {
-                    SatsadePix = SatsadePix *= 3;
-                    pix = pix += SatsadePix;
+                    else if (tärning1 == ValtLyckoTal && tärning2 == ValtLyckoTal || tärning2 == ValtLyckoTal && tärning3 == ValtLyckoTal || tärning3 == ValtLyckoTal && tärning2 == ValtLyckoTal || tärning3 == ValtLyckoTal && tärning3 == ValtLyckoTal)
+                    {
+                        SatsadePix = SatsadePix *= 3;
+                        pix = pix += SatsadePix;
 
-                    Console.WriteLine("Du har Vunnit 3x " + "Pix kvar: " + pix);
-                    Console.WriteLine("Tärning 1; " + tärning1 + " Tärning2; " + tärning2 + " Tärning3; " + tärning2);
-                }
-                
-                else if (tärning1 == ValtLyckoTal || tärning2 == ValtLyckoTal || tärning3 == ValtLyckoTal)
-                {
-                    SatsadePix = SatsadePix *= 2;
-                    pix = pix += SatsadePix;
+                        Console.WriteLine("Du har Vunnit 3x " + "Pix kvar: " + pix);
+                        Console.WriteLine("Tärning 1; " + tärning1 + " Tärning2; " + tärning2 + " Tärning3; " + tärning2);
+                    }
 
-                    Console.WriteLine("Du har Vunnit 2x " + "Pix kvar: " + pix);
-                    Console.WriteLine("Tärning 1; " + tärning1 + " Tärning2; " + tärning2 + " Tärning3; " + tärning2);
+                    else if (tärning1 == ValtLyckoTal || tärning2 == ValtLyckoTal || tärning3 == ValtLyckoTal)
+                    {
+                        SatsadePix = SatsadePix *= 2;
+                        pix = pix += SatsadePix;
 
+                        Console.WriteLine("Du har Vunnit 2x " + "Pix kvar: " + pix);
+                        Console.WriteLine("Tärning 1; " + tärning1 + " Tärning2; " + tärning2 + " Tärning3; " + tärning2);
+
+
+                    }
+
+                    else
+                    {
+                        pix = pix -= SatsadePix;
+                        Console.WriteLine("Du har förlorat " + "Pix kvar: " + pix);
+
+                        Console.WriteLine("Tärning 1; " + tärning1 + " Tärning2; " + tärning2 + " Tärning3; " + tärning2);
+                        Console.WriteLine("Pengar Kvar: " + pix);
+
+                    }
 
                 }
 
                 else
                 {
-                    pix = pix -= SatsadePix;
-                    Console.WriteLine("Du har förlorat " + "Pix kvar: " + pix);
+                    Console.WriteLine("Du angivit ett för stort eller litet tal");
+                    Console.WriteLine("Tärning 1; " + tärning1 + " Tärning2; " + tärning2 + " Tärning2; " + tärning2);
 
-                    Console.WriteLine("Tärning 1; " + tärning1 + " Tärning2; " + tärning2 + " Tärning3; " + tärning2);
-                    Console.WriteLine("Pengar Kvar: " + pix);
 
                 }
-
             }
-
-            else
-            {
-                Console.WriteLine("Du angivit ett för stort eller litet tal");
-                Console.WriteLine("Tärning 1; " + tärning1 + " Tärning2; " + tärning2 + " Tärning2; " + tärning2);
-                
-
-            }
-
-
         }
-        static void PengKvarOchTärningar(int tärning1, int tärning2, int tärning3, int pix)
-        {
 
-            Console.WriteLine("Tärning 1; " + tärning1 + " Tärning2; " + tärning2 + " Tärning2; " + tärning2);
-            Console.WriteLine("Pengar Kvar: " + pix);
-        }
+
     }
+
+    
 }
