d23f79abdc0a472c15491c1061e8f86ed8ae1ca8
AjajajOnt <alioch005@hotmail.com>
2021-09-18 17:11:59 +0200

    nästan klar?

--- Inläming 1 - Tärningar/Program.cs
+++ Inläming 1 - Tärningar/Program.cs
@@ -10,9 +10,10 @@ namespace Inläming_1___Tärningar
             int pix = 500;
             int ValtLyckoTal = 0;
             int SatsadePix = 0;
+            string sant = "ja";
 
 
-            while (pix >= 50)
+            while (pix >= 50 &&  sant == "ja")
             {
 
                 int tärning1 = rnd.Next(1, 6);
@@ -20,21 +21,25 @@ namespace Inläming_1___Tärningar
                 int tärning3 = rnd.Next(1, 6);
                 Console.WriteLine("Välj ett lyckotal mellan 1 och 6");
                 ValtLyckoTal = int.Parse(Console.ReadLine());
-                Console.WriteLine("Satsa Pix");
+                Console.WriteLine("Du har: " + pix + " Pix ");
+                Console.WriteLine("Satsa Pix:");
                 SatsadePix = int.Parse(Console.ReadLine());
 
                 if (SatsadePix >= 50 && SatsadePix <= pix && ValtLyckoTal >= 1 && ValtLyckoTal <= 6)
 
+
                 {
+                    
 
                     if (tärning1 == ValtLyckoTal && tärning2 == ValtLyckoTal && tärning3 == ValtLyckoTal)
                     {
 
+
                         SatsadePix = SatsadePix *= 4;
+                        Console.WriteLine("Du har Vunnit: " + SatsadePix + " Pix");
                         pix = pix += SatsadePix;
 
-                        Console.WriteLine("Du har Vunnit 4x " + "Pix kvar: " + pix);
-                        Console.WriteLine("Tärning 1; " + tärning1 + " Tärning2; " + tärning2 + " Tärning3; " + tärning2);
+                        Console.WriteLine("Pix kvar: " + pix);
 
 
                     }
@@ -42,30 +47,29 @@ namespace Inläming_1___Tärningar
                     else if (tärning1 == ValtLyckoTal && tärning2 == ValtLyckoTal || tärning2 == ValtLyckoTal && tärning3 == ValtLyckoTal || tärning3 == ValtLyckoTal && tärning2 == ValtLyckoTal || tärning3 == ValtLyckoTal && tärning3 == ValtLyckoTal)
                     {
                         SatsadePix = SatsadePix *= 3;
+                        Console.WriteLine("Du har Vunnit: " + SatsadePix + " Pix");
                         pix = pix += SatsadePix;
 
-                        Console.WriteLine("Du har Vunnit 3x " + "Pix kvar: " + pix);
-                        Console.WriteLine("Tärning 1; " + tärning1 + " Tärning2; " + tärning2 + " Tärning3; " + tärning2);
+                        Console.WriteLine("Pix kvar: " + pix);
                     }
 
                     else if (tärning1 == ValtLyckoTal || tärning2 == ValtLyckoTal || tärning3 == ValtLyckoTal)
                     {
+
                         SatsadePix = SatsadePix *= 2;
+                        Console.WriteLine("Du har Vunnit: " + SatsadePix + " Pix");
                         pix = pix += SatsadePix;
 
-                        Console.WriteLine("Du har Vunnit 2x " + "Pix kvar: " + pix);
-                        Console.WriteLine("Tärning 1; " + tärning1 + " Tärning2; " + tärning2 + " Tärning3; " + tärning2);
+                        Console.WriteLine("Pix kvar: " + pix);
 
 
                     }
 
                     else
                     {
+                        Console.WriteLine("Du har förlorat: " + SatsadePix + " Pix");
                         pix = pix -= SatsadePix;
-                        Console.WriteLine("Du har förlorat " + "Pix kvar: " + pix);
-
-                        Console.WriteLine("Tärning 1; " + tärning1 + " Tärning2; " + tärning2 + " Tärning3; " + tärning2);
-                        Console.WriteLine("Pengar Kvar: " + pix);
+                        Console.WriteLine("Pix Kvar: " + pix);
 
                     }
 
@@ -74,10 +78,24 @@ namespace Inläming_1___Tärningar
                 else
                 {
                     Console.WriteLine("Du angivit ett för stort eller litet tal");
-                    Console.WriteLine("Tärning 1; " + tärning1 + " Tärning2; " + tärning2 + " Tärning2; " + tärning2);
 
 
                 }
+                
+
+                if (pix >= 50)
+                {
+                    
+                    Console.WriteLine("Vill du spel igen? " + "Skriv ja om du vill köra igen. ");
+                    
+                    sant = Console.ReadLine();
+                    Console.Clear();
+
+                }
+                
+
+
+
             }
         }
 
